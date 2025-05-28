da2b98807d27623d2a765c9235b6b0ba5d5be2b0
matteo-brandolino <matteo.brandolino@gmail.com>
2025-05-28 22:10:25 +0200

    two crystall balls solution

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpValue = Math.floor(Math.sqrt(breaks.length));
+    let index = jumpValue;
+    for (; index < breaks.length; index += jumpValue) {
+        const found = breaks[index];
+        if (found) break;
+    }
+    // go back from found value
+    index -= jumpValue;
 
+    for (let j = 0; j <= jumpValue && index < breaks.length; ++j, ++index) {
+        const found = breaks[index];
+        if (found) return index;
+    }
+    return -1;
 }
